f85b3dab3dd536720354ebcbf3a1b22a014c76fc
Carolina <caroonava21@gmail.com>
2023-10-05 12:59:05 -0300

    final

--- src/view.js
+++ src/view.js
@@ -30,7 +30,7 @@ export const renderItems = (data) => {
     // Crear un elemento año de publicación y establecer su contenido
     const year = document.createElement("h4");
     year.textContent = item.release_date;
-    year.setAttribute("itemprop", "datePublished"); // Agregar el atributo itemprop
+    year.setAttribute("itemprop", "release_date"); // Agregar el atributo itemprop
 
     // Añadir la imagen, título y año al elemento tarjeta
     card.appendChild(image);
